a50d06ab234e28fc3c193fdd08830d63900727b9
MrJaqbq <bentkowski.jakub@gmail.com>
2017-01-04 13:50:48 +0100

    Map Pickup

---
+++ Assets/Scripts/MapPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPickup : GenericPickup
+{
+    public MiniMapController MiniMap;
+
+    private void Start()
+    {
+        if (!MiniMap)
+        {
+            MiniMap = FindObjectOfType<MiniMapController>();
+        }
+    }
+
+    protected override void OnPickup(Pawn pawn)
+    {
+        MiniMap.DisplayUndiscovered = true;
+    }
+}
